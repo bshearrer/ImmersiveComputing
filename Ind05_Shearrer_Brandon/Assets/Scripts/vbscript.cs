268044a5c8442fc94349db9a874d39ead629a825
Brandon Shearrer <bshearr@outlook.com>
2020-04-16 16:46:48 -0500

    VB Implement & Cube Animation
    When pressing vb on one image, the cube spins on the other. Releasing hand from button stops cube animation.

---
+++ Ind05_Shearrer_Brandon/Assets/Scripts/vbscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class vbscript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbutton;
+    public Animator animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbutton = GameObject.Find("VirtualButton");
+        vbutton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        animation.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        Debug.Log("Button Pressed!");
+        animation.Play("cube_animation");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+        Debug.Log("Button Released.");
+        animation.Play("none");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
