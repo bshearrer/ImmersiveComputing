6c60177b506bbe7895a8ed205c89e86337e24192
Brandon Shearrer <bshearr@outlook.com>
2020-04-18 17:28:43 -0500

    Door and Button Complete
    Everything done, except scene added behind doors

--- Ind05_Shearrer_Brandon/Assets/Scripts/vbscript.cs
+++ Ind05_Shearrer_Brandon/Assets/Scripts/vbscript.cs
@@ -1,27 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Vuforia;
 public class vbscript : MonoBehaviour, IVirtualButtonEventHandler
 {
     public GameObject vbutton;
-    public Animator animation;
+    public GameObject buttonText;
+    public Animator right_door_ani;
+    public Animator left_door_ani;
+
+    private bool doorsOpen = false;
+
     // Start is called before the first frame update
     void Start()
     {
         vbutton = GameObject.Find("VirtualButton");
+        
         vbutton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-        animation.GetComponent<Animator>();
+        right_door_ani.GetComponent<Animator>();
+        left_door_ani.GetComponent<Animator>();
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb){
         Debug.Log("Button Pressed!");
-        animation.Play("cube_animation");
+        
+
+        //Code runs if opening doors
+        if(doorsOpen == false){
+            buttonText.GetComponent<TextMesh>().text = "Press to\nClose Doors";
+            buttonText.GetComponent<TextMesh>().color = Color.red;
+            right_door_ani.Play("rd_open");
+            left_door_ani.Play("ld_open");
+            doorsOpen = true;
+        }
+        //Code runs if closing doors
+        else if(doorsOpen == true) {
+            buttonText.GetComponent<TextMesh>().text = "Press to\nOpen Doors";
+            buttonText.GetComponent<TextMesh>().color = Color.green;
+            left_door_ani.Play("ld_close");
+            right_door_ani.Play("rd_close");
+            doorsOpen = false;
+        }
+
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb){
         Debug.Log("Button Released.");
-        animation.Play("none");
     }
     // Update is called once per frame
     void Update()
