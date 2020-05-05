6f26346989e181626a0e9f3618e66b74c13b1e7e
Brandon Shearrer <37765856+bshearrer@users.noreply.github.com>
2020-05-05 02:50:06 -0500

    Project 6 Complete *With Waypoints

---
+++ Ind06_Shearrer_Brandon/Assets/Scripts/PlayerAI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAI : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoints;
+    private int targetWaypointIndex;
+    private float minDistance = 0.1f;
+    private int lastWaypointIndex;
+
+    public float movementSpeed = 0.5f;
+
+    public GameObject objToTP;
+    public Transform tpLoc1;
+    public Transform tpLoc2;
+
+    private int randTP = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastWaypointIndex = waypoints.Count - 1;
+        targetWaypoints = waypoints[targetWaypointIndex];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementStep = movementSpeed * Time.deltaTime;
+
+        float distance = Vector3.Distance(transform.position, targetWaypoints.position);
+        CheckDistanceToWaypoint(distance);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoints.position, movementStep);
+
+    }
+    
+    void CheckDistanceToWaypoint(float currentDistance)
+    {
+        if(currentDistance <= minDistance)
+        {
+            if(randTP == 0)
+            {
+                objToTP.transform.position = tpLoc1.transform.position;
+                randTP = 1;
+            }
+            else
+            {
+                objToTP.transform.position = tpLoc2.transform.position;
+                randTP = 0;
+            }
+            targetWaypointIndex++;
+            UpdateTargetWaypoint();
+        }
+    }
+
+    void UpdateTargetWaypoint()
+    {
+        if(targetWaypointIndex > lastWaypointIndex)
+        {
+            targetWaypointIndex = 0;
+        }
+        targetWaypoints = waypoints[targetWaypointIndex];
+    }
+}
---
+++ Ind06_Shearrer_Brandon/Assets/Scripts/vbscript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+using Vuforia;
+public class vbscript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbutton;
+    public GameObject buttonText;
+    public Animator right_door_ani;
+    public Animator left_door_ani;
+
+    public AudioClip doorOpen;
+    public AudioClip doorClose;
+    public AudioSource source;
+
+    private bool doorsOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbutton = GameObject.Find("VirtualButton");
+
+        vbutton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        right_door_ani.GetComponent<Animator>();
+        left_door_ani.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        Debug.Log("Button Pressed!");
+
+        
+        //Code runs if opening doors
+        if(doorsOpen == false){
+
+            buttonText.GetComponent<TextMesh>().text = "Press to\nClose Doors";
+            buttonText.GetComponent<TextMesh>().color = Color.red;
+            right_door_ani.Play("rd_open");
+            left_door_ani.Play("ld_open");
+            source.PlayOneShot(doorOpen);
+            doorsOpen = true;
+        }
+        //Code runs if closing doors
+        else if(doorsOpen == true) {
+            buttonText.GetComponent<TextMesh>().text = "Press to\nOpen Doors";
+            buttonText.GetComponent<TextMesh>().color = Color.green;
+            left_door_ani.Play("ld_close");
+            right_door_ani.Play("rd_close");
+            source.PlayOneShot(doorClose);
+            doorsOpen = false;
+        }
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb){
+        Debug.Log("Button Released.");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
